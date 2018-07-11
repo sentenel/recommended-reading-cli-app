706346f0311edaf6b2c31e029f2d1776f5015460
Seth Wax <sethlwax@gmail.com>
2018-07-11 20:30:35 +0000

    Added structure for initial CLI loop.

--- lib/recommended_reading/cli.rb
+++ lib/recommended_reading/cli.rb
@@ -1,9 +1,29 @@
+require 'pry'
+
 class RecommendedReading::CLI
 
-  puts "here"
+  LISTS = ["Amazon Bestsellers", "New York Times Bestsellers", "Barnes and Noble Bestsellers"]
 
   def call
-    puts "hello"
+    while true
+      puts "Please enter the number of the booklist you would like to view"
+      LISTS.each.with_index {|list, index| puts "#{index + 1}. #{list}"}
+
+      input = gets.strip
+
+      break if ["Q", "QUIT", "EXIT"].include?(input.upcase)
+
+      case input.to_i
+      when 1
+        puts LISTS[0]
+      when 2
+        puts LISTS[1]
+      when 3
+        puts LISTS[2]
+      else
+        puts "I did not understand."
+      end
+    end
   end
 
 end
