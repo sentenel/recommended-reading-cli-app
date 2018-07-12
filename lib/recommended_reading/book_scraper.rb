84387012bd5a831ddc0c4f250a3701bd4a124d5b
Seth Wax <sethlwax@gmail.com>
2018-07-12 08:33:44 +0000

    Commented in locations on goodreads to scrape Title and Author and summary.

--- lib/recommended_reading/book_scraper.rb
+++ lib/recommended_reading/book_scraper.rb
@@ -1,7 +1,12 @@
+require 'pry'
 class RecommendedReading::BookScraper
 
   def scrape_goodreads(isbn)
     doc = Nokogiri::HTML(open("https://www.goodreads.com/book/isbn/#{isbn}"))
+    binding.pry
+    #title: doc.css('h1.bookTitle').text.strip
+    #authors: doc.css('div#bookAuthors').text.gsub(/( by)*?/, '').strip
+    #summary: doc.css('div#description').text.gsub(/\.\.\.more/, '').strip
   end
 
 end
