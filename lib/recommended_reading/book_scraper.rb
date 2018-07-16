18eaf743536a943b6d4c21d3b9058b4aa56ba0bd
Seth Wax <sethlwax@gmail.com>
2018-07-16 13:09:01 +0000

    Scraping link for goodreads ratings.

--- lib/recommended_reading/book_scraper.rb
+++ lib/recommended_reading/book_scraper.rb
@@ -7,6 +7,9 @@ class RecommendedReading::BookScraper
     #title: doc.css('h1.bookTitle').text.strip
     #authors: doc.css('div#bookAuthors').text.gsub(/( by)*?/, '').strip
     #summary: doc.css('div#description').text.gsub(/\.\.\.more/, '').strip
+    #genres: doc.css('div.left a.bookPageGenreLink')[index].text
+    #ratings array: doc.css('div#bookMeta script').first.children.first.content.scan(/d+ ratings/)
+
   end
 
 end
