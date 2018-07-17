1b116d886e084f22560574ddbe280f264f237126
Seth Wax <sethlwax@gmail.com>
2018-07-17 10:27:19 +0000

    Added link to amazon bestsellers titles to scraper.

--- lib/recommended_reading/list_scraper.rb
+++ lib/recommended_reading/list_scraper.rb
@@ -4,6 +4,7 @@ class RecommendedReading::ListScraper
   end
 
   def scrape_amazon_bestsellers
+    #title doc.css("div.p13n-sc-truncate.p13n-sc-line-clamp-1")[index].text.strip
   end
 
   def scrape_barnes_and_noble_bestsellers
