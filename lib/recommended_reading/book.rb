24426dbde3533c5906b777477df46c53f1190120
Seth Wax <sethlwax@gmail.com>
2018-07-17 08:22:59 +0000

    Updated rating methods, added reviews getter.

--- lib/recommended_reading/book.rb
+++ lib/recommended_reading/book.rb
@@ -7,16 +7,20 @@ class RecommendedReading::Book
 
   def average_rating
     if ratings.length > 0
-      combined = ratings.inject([0, 0]) {|total, rating| [total[0] + rating[0] * rating[1], total[1] + rating[1]]}
-      (combined[0] / combined[1].to_f).round(2)
+      weighted = ratings.map.with_index {|rating, index| rating * (5 - index)}.reduce(:+)
+      (weighted / ratings.reduce(:+).to_f).round(2)
     else
       "No ratings available"
     end
   end
 
   def total_ratings
-    ratings.each {|rating| puts "#{rating[0]} star ratings: #{rating[1]}"}
-    puts "Total ratings: #{ratings.inject(0){|total, rating| total + rating[1]}}"
+    ratings.each_with_index {|number, index| puts "#{5-index} star ratings: #{number}"}
+    puts "Total ratings: #{ratings.reduce(:+)}"
+  end
+
+  def reviews
+    reviews ||= RecommendedReading::BookScraper.scrape_goodreads_reviews(isbn)
   end
 
   def self.new_from_goodreads(isbn)
