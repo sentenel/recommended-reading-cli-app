bcc536c3b356849812fc7eb099474bd024d00712
Seth Wax <sethlwax@gmail.com>
2018-07-17 02:57:05 +0000

    Goodreads scraper returns hash containing book information.

--- lib/recommended_reading/book_scraper.rb
+++ lib/recommended_reading/book_scraper.rb
@@ -3,12 +3,14 @@ class RecommendedReading::BookScraper
 
   def scrape_goodreads(isbn)
     doc = Nokogiri::HTML(open("https://www.goodreads.com/book/isbn/#{isbn}"))
-    binding.pry
-    #title: doc.css('h1.bookTitle').text.strip
-    #authors: doc.css('div#bookAuthors').text.gsub(/( by)*?/, '').strip
-    #summary: doc.css('div#description').text.gsub(/\.\.\.more/, '').strip
-    #genres: doc.css('div.left a.bookPageGenreLink')[index].text
-    #ratings array: doc.css('div#bookMeta script').first.children.first.content.scan(/d+ ratings/)
+    Hash.new.tap do |book|
+      book[:title] = doc.css('h1.bookTitle').text.strip
+      book[:authors] = doc.css('div#bookAuthors').text.gsub(/( by)*?/, '').strip.gsub(/by\n+/, '')
+      book[:summary] = doc.css('div#description').text.gsub(/\.\.\.more/, '').strip
+      book[:genres] = doc.css('div.left a.bookPageGenreLink').map {|element| element.text}
+      ratings = doc.css('div#bookMeta script').first.children.first.content.scan(/\d+ ratings/)
+      book[:ratings] =ratings.map {|total| total.gsub(/ ratings/, '').to_i}
+    end
   end
 
   def scrape_goodreads_review(isbn)
