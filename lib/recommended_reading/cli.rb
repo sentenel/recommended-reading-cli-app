c4edd49a0006ccb985c09f5fd253f8d2afd9a264
Seth Wax <sethlwax@gmail.com>
2018-07-17 08:25:40 +0000

    Aesthetic/Formatting updates, now uses Book class for data.

--- lib/recommended_reading/cli.rb
+++ lib/recommended_reading/cli.rb
@@ -1,16 +1,10 @@
+require 'pry'
 class RecommendedReading::CLI
 
-  LISTS = ["Amazon Bestsellers", "New York Times Bestsellers", "Barnes and Noble Bestsellers"]
+  LISTS = ["Amazon Bestsellers", "New York Times Bestsellers", "Barnes and Noble Bestsellers", "Publishers Weekly"]
 
   def call
-    testbook = RecommendedReading::Book.new('006242713X')
-    testbook.title = "The Plant Paradox"
-    testbook.author = "Steven R. Gundry"
-    testbook.ratings = [[3.84, 1]]
-    testbook.genres = ["Health", "Nonfiction", "Food and Drink > Food", "Science", "Health > Nutrition", "Self Help"]
-    testbook.summary = 'From renowned cardiac surgeon Steven R. Gundry, MD, a revolutionary look at the hidden compounds in "healthy" foods like fruit, vegetables, and whole grains that are causing us to gain weight and develop chronic disease.'
-    testbook.reviews = [[1, "To put it generously, I am not the intended audience for a book like this, and I would not normally seek out, let alone read, a diet book. Nonetheless..."]]
-    testbook.quotes = ["The most dangerous trick pulled by lectins, which I now see on a daily basis in my patients, is that they bear an uncanny similarity to the proteins on many of our important organs, nerves, and joints."]
+    testbook = RecommendedReading::Book.new_from_goodreads('006242713X')
 
     puts "Please enter the number of the booklist you would like to view"
     LISTS.each.with_index {|list, index| puts "#{index + 1}. #{list}"}
@@ -25,6 +19,8 @@ class RecommendedReading::CLI
         display_list ["The President is Missing"]
       when 3
         display_list ["Clock Dance"]
+      when 4
+        display_list["stub"]
       else
         puts "I did not understand."
         call
@@ -48,34 +44,34 @@ class RecommendedReading::CLI
   end
 
   def book_details(book)
-    puts "#{book.title} by #{book.author}"
+    puts "#{book.title} by #{book.authors}"
     puts "Average rating: #{book.average_rating}"
     puts "Genres: #{book.genres}"
-    puts ""
-    puts book.summary
 
     input = nil
     while input != 5
       puts ""
       puts "What would you like to see?"
-      puts "1. Reviews"
-      puts "2. Quotes"
+      puts "1. Summary"
+      puts "2. Reviews"
       puts "3. Recommendations based on this book"
       puts "4. Rating information"
       puts "5. View another list"
 
       input = gets.strip.to_i
+      puts ""
 
       case input
       when 1
+        book.summary
+      when 2
         puts "#{book.reviews[0][0]} stars"
         puts book.reviews[0][1]
-      when 2
-        puts book.quotes[0]
       when 3
         puts "Recommendation stub"
       when 4
-        puts "Rating information stub"
+        book.total_ratings
+        puts "#Average rating: #{book.average_rating}"
       when 5
         break
       else
