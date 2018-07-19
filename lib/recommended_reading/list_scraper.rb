d44a214dab6f985bef63b86df5f4248a2da51fe5
Seth Wax <sethlwax@gmail.com>
2018-07-19 11:05:49 +0000

    Added amazon bestsellers scraper

--- lib/recommended_reading/list_scraper.rb
+++ lib/recommended_reading/list_scraper.rb
@@ -4,7 +4,16 @@ class RecommendedReading::ListScraper
   end
 
   def scrape_amazon_bestsellers
-    #title doc.css("div.p13n-sc-truncate.p13n-sc-line-clamp-1")[index].text.strip
+    list_length = 20
+    doc = Nokogiri::HTML(open("https://www.amazon.com/bestsellers/books"))
+    Array.new.tap do |books|
+      (0..list_length-1).each do |index|
+      books << {
+        title: doc.css("div.p13n-sc-truncate.p13n-sc-line-clamp-1")[index].text.strip,
+        link: "https://www.amazon.com" + doc.xpath("//span[@class='a-size-base a-color-price']/..")[index]['href']
+      }
+      end
+    end
   end
 
   def scrape_barnes_and_noble_bestsellers
